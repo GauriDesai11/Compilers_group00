6ffcd254ae24af6e965c45ab0877e86711baaf76
GauriDesai11 <gauri.desai.21@ucl.ac.uk>
2024-04-01 10:38:46 +0100

    made code more modular, need to optimize other arithmetic operations

--- src/comp0012/main/ConstantFolder.java
+++ src/comp0012/main/ConstantFolder.java
@@ -29,151 +29,113 @@ public class ConstantFolder
 		}
 	}
 
+	public class Result
+	{
+		private InstructionList default_il;
+		private ConstantPoolGen default_cpgen;
+	}
+
+	private Result replaceInstructions(InstructionList il, InstructionHandle ih, InstructionHandle prev1, InstructionHandle prev2, Number result, ConstantPoolGen cpg)
+	{
+		// Assuming result is always an integer for simplicity; adjust for other types
+		int index = cpg.addInteger(result.intValue());
+		il.insert(ih, new LDC(index));
+
+		try {
+			il.delete(ih);
+			il.delete(prev1);
+			il.delete(prev2);
+		} catch (TargetLostException e) {
+			throw new RuntimeException(e);
+		}
+		Result toReturn = new Result();
+		toReturn.default_il = il;
+		toReturn.default_cpgen = cpg;
+
+		return toReturn;
+	}
+
+	private Number performOperation(Object obj1, Object obj2, Instruction inst)
+	{
+		if (inst instanceof IADD && obj1 instanceof Integer && obj2 instanceof Integer) {
+			return (Integer) obj1 + (Integer) obj2;
+		}
+		// Add more conditions for other types and operations (ISUB, IMUL, IDIV, etc.)
+		return 0;
+	}
+
+	public Result handleArithmetic(InstructionHandle ih, InstructionList instructionList, ConstantPoolGen cpgen)
+	{
+		Result returned = new Result();
+		returned.default_il = instructionList;
+		returned.default_cpgen = cpgen;
+
+		InstructionHandle prev1 = ih.getPrev();
+		InstructionHandle prev2 = prev1 != null ? prev1.getPrev() : null;
+		if (prev1 != null && prev2 != null) {
+			Instruction prevInst1 = prev1.getInstruction();
+			Instruction prevInst2 = prev2.getInstruction();
+
+			if (prevInst1 instanceof LDC && prevInst2 instanceof LDC) {
+				Object obj1 = ((LDC) prevInst1).getValue(cpgen);
+				Object obj2 = ((LDC) prevInst2).getValue(cpgen);
+
+				if (obj1.getClass().equals(obj2.getClass())) {
+					Number result = performOperation(obj1, obj2, ih.getInstruction());
+					returned = replaceInstructions(instructionList, ih, prev1, prev2, result, cpgen);
+				}
+			}
+		}
+		return returned;
+	}
+
+	public ConstantPoolGen SimpleFoldingOptimization(ConstantPoolGen cpgen)
+	{
+		Method[] methods = gen.getMethods();
+		Result returned = new Result();
+
+		for (Method method : methods) {
+
+			MethodGen methodGen = new MethodGen(method, gen.getClassName(), cpgen);
+			InstructionList instructionList = methodGen.getInstructionList();
+
+			returned.default_il = instructionList;
+			returned.default_cpgen = cpgen;
+
+			if (instructionList != null) {
+				for (InstructionHandle ih : instructionList.getInstructionHandles()) {
+
+					Instruction instruction = ih.getInstruction();
+
+					if (instruction instanceof ArithmeticInstruction)
+					{
+						returned = handleArithmetic(ih, instructionList, cpgen);
+					}
+				}
+
+				methodGen.setInstructionList(returned.default_il);
+				methodGen.setMaxStack(); //stack frame size needs to be correctly calculated and set
+				methodGen.setMaxLocals();
+				gen.replaceMethod(method, methodGen.getMethod()); //'method' = original method --> replace this with the new method with the updated InstructionList
+			}
+		}
+		return returned.default_cpgen;
+	}
+
 	public void optimize()
 	{
 		ClassGen cgen = new ClassGen(original);
 		ConstantPoolGen cpgen = cgen.getConstantPool();
 
 		// Implement your optimization here
-		//System.out.println("optimising general\n");
-		//System.out.println("class name: " + cgen.getClassName() + "\n");
-
-
 
 		if ((cgen.getClassName()).equals("comp0012.target.SimpleFolding"))
 		{
-			//System.out.println("optimising simple folding\n");
-
-			Method[] methods = gen.getMethods();
-			int count = 0;
-
-			for (Method method : methods) {
-				//System.out.println("method\n");
-
-				/*
-				if (count == 0)
-				{
-					count++;
-					System.out.println("skipping the 1st method");
-					continue;
-				}
-				*/
-
-				MethodGen methodGen = new MethodGen(method, gen.getClassName(), cpgen);
-				InstructionList instructionList = methodGen.getInstructionList();
-
-
-				System.out.println(" ########### printing all directly copied original instructions ########### \n");
-
-				for (InstructionHandle ih : instructionList.getInstructionHandles()) {
-					Instruction inst = ih.getInstruction();
-					System.out.println(inst.toString()+ "\n");
-				}
-
-				System.out.println(" ########### printing all directly copied constants ########### \n");
-				ConstantPool cp = cpgen.getConstantPool();
-				for (int i = 0; i < cp.getLength(); i++) {
-					Constant constant = cp.getConstant(i);
-					if (constant != null) {
-						System.out.println(i + ": " + constant + "\n");
-					}
-				}
-
-				if (instructionList != null) {
-					for (InstructionHandle ih : instructionList.getInstructionHandles()) {
-						//System.out.println("instruction\n");
-
-						Instruction instruction = ih.getInstruction();
-
-						// Check for arithmetic instructions
-						if (instruction instanceof IADD)
-						{
-							InstructionHandle prev1 = ih.getPrev();
-							InstructionHandle prev2 = prev1 != null ? prev1.getPrev() : null;
-							if (prev1 != null && prev2 != null) {
-								Instruction prevInst1 = prev1.getInstruction();
-								Instruction prevInst2 = prev2.getInstruction();
-
-								if ((prevInst1 instanceof LDC) && (prevInst2 instanceof LDC))
-								{
-									//System.out.println("found 2 loading instructions \n");
-
-									Object obj1 = ((LDC) prevInst1).getValue(cpgen);
-									Object obj2 = ((LDC) prevInst2).getValue(cpgen);
-
-									if ((obj1 instanceof Integer) && (obj2 instanceof Integer))
-									{
-
-										//System.out.println("adding 2 integers\n");
-
-										int cnstValue = (Integer) obj1 + (Integer) obj2;
-										System.out.println("number: " + cnstValue + "\n");
-
-										int index = cpgen.addInteger(cnstValue);
-										// Load the calculated value onto the stack
-										instructionList.insert(ih, new LDC(index));
-
-										//instructionList.insert(ih, new PUSH(cpgen, cnstValue)); //adding the instruction at the correct handle
-										//System.out.println("added new instruction\n");
-
-										try{
-											instructionList.delete(ih);
-											//System.out.println("deleted add\n");
-										} catch (TargetLostException e) {
-											throw new RuntimeException(e);
-										}
-										try {
-											instructionList.delete(prev1);
-											//System.out.println("deleted 1st load\n");
-										} catch (TargetLostException e) {
-											throw new RuntimeException(e);
-										}
-										try {
-											instructionList.delete(prev2);
-											//System.out.println("deleted 2nd load\n");
-										}  catch (TargetLostException e) {
-											throw new RuntimeException(e);
-										}
-									}
-								}
-							}
-						}
-					}
-
-					System.out.println(" ########### printing all new edited instructions ########### \n");
-
-					for (InstructionHandle ih : instructionList.getInstructionHandles()) {
-						Instruction inst = ih.getInstruction();
-						System.out.println(inst.toString()+ "\n");
-					}
-
-					System.out.println(" ########### printing all new constants ########### \n");
-					ConstantPool cp2 = cpgen.getConstantPool();
-					for (int i = 0; i < cp2.getLength(); i++) {
-						Constant constant = cp2.getConstant(i);
-						if (constant != null) {
-							System.out.println(i + ": " + constant + "\n");
-						}
-					}
-
-
-					methodGen.setInstructionList(instructionList);
-					//System.out.println("updated the instructionList\n");
-					methodGen.setMaxStack(); //stack frame size needs to be correctly calculated and set
-					//System.out.println("set the stack\n");
-					methodGen.setMaxLocals();
-					//System.out.println("set the locals\n");
-					gen.replaceMethod(method, methodGen.getMethod()); //'method' = original method --> replace this with the new method with the updated InstructionList
-					//System.out.println("replaced the method\n");
-				}
-			}
+			cpgen = SimpleFoldingOptimization(cpgen);
 		}
-
 
 		gen.setConstantPool(cpgen);
 		this.optimized = gen.getJavaClass(); //line from the original code
-		//ConstantPool cp3 = cpgen.getConstantPool();
-		//this.optimized.setConstantPool(cp3);
 	}
 
 
